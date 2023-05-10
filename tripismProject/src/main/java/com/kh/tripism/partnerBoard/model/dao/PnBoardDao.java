3f74495bde87fbf51670cd8763bba51c58485ac4
soominmin <108602594+soominmin@users.noreply.github.com>
2023-05-10 15:12:17 +0900

    pnBoard Controller/dao.service

--- tripismProject/src/main/java/com/kh/tripism/partnerBoard/model/dao/PnBoardDao.java
+++ tripismProject/src/main/java/com/kh/tripism/partnerBoard/model/dao/PnBoardDao.java
@@ -1,8 +1,105 @@
 package com.kh.tripism.partnerBoard.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.tripism.feed.model.vo.PageInfo;
+import com.kh.tripism.partnerBoard.model.vo.PnBoard;
+import com.kh.tripism.partnerBoard.model.vo.PnReply;
+
+@Repository
 public class PnBoardDao {
-	// 동행 dao
+	
+	// 동행관련 조회수 
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("pnBoardMapper.selectListCount");
+	}
+	
+	// 동행관련 리스트 조회
+	public ArrayList<PnBoard> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+		
+		int offset = (pi.getCurrentPage()-1) * pi.getBoardLimit();	// 몇개 건너 뛸 건지
+		int limit = pi.getBoardLimit();	// 총 몇개를 조회할건지
+		
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		
+		return (ArrayList)sqlSession.selectList("pnBoardMapper.selectList", null, rowBounds);	
+	}
 	
 	
+	// 동행관련 게시글추가
+	public int insertPnBoard(SqlSessionTemplate sqlSession, PnBoard pb) {
+		return sqlSession.insert("pnBoardMapper.insertPnBoard", pb);
+	}
+	
+	
+	// 동행 관련 조회수 증가
+	public int increaseCount(SqlSessionTemplate sqlSession, int pnBoardNo) {
+		return sqlSession.update("pnBoardMapper.increaseCount", pnBoardNo);
+	}
+	
+	
+	// 동행 관련 동행 조회
+	public PnBoard selectPnBoard(SqlSessionTemplate sqlSession, int pnBoardNo) {
+		return sqlSession.selectOne("pnBoardMapper.selectPnBoard", pnBoardNo);
+	}
+	
+	
+	// 동행 관련 동행 삭제
+	public int deletePnBoard(SqlSessionTemplate sqlSession, int pnBoardNo) {
+		return sqlSession.selectOne("pnBoardMapper.deletePnBoard", pnBoardNo);
+	}
+	
+	
+	// 동행 관련 동행 수정
+	public int updatePnBoard(SqlSessionTemplate sqlSession, PnBoard pb) {
+		return sqlSession.update("pnBoardMapper.updatePnBoard", pb);
+	}
+	
+	
+	// 동행 관련 댓글
+	public ArrayList<PnReply> selectPnReplyList(SqlSessionTemplate sqlSession, int pnBoardNo) {
+		return (ArrayList)sqlSession.selectList("pnBoardMapper.selectPnReplyList", pnBoardNo);
+	}
+	
+	// 동행 관련 댓글 추가
+	public int insertPnReply(SqlSessionTemplate sqlSession, PnReply pr) {
+		return sqlSession.insert("pnBoardMapper.insertPnReply", pr);
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tripismProject/src/main/java/com/kh/tripism/partnerBoard/model/vo/PnBoard.java
+++ tripismProject/src/main/java/com/kh/tripism/partnerBoard/model/vo/PnBoard.java
@@ -13,12 +13,12 @@ import lombok.ToString;
 @ToString
 public class PnBoard {
 	
-	private int postNO;						// 게시글번호
+	private int postNo;						// 게시글번호
 	private int memNo; 						// 회원번호
 	private String partnerName;	 			// 글제목	
 	private String partnerType;	 			// 여행타입
 	private String partnerOriginalImg; 		// 대표이미지 원본이름
-	private String partnerChangeImg; 		// 대표이미지 변경이름
+	private String partnerChangeImg; 		// 대표이미지 변경이름 "resources/uploadFiles/xxx.jpg" 이렇게 넣을거임 
 	private Date partnerDate; 				// 작성일
 	private String partnerViews; 			// 조회수	
 	private String partnerContent; 			// 글내용

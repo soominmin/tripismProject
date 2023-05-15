47f24a79d850918957e19b3a209bf785c12ac02a
hhye2 <116607632+donghyun1597@users.noreply.github.com>
2023-05-15 15:06:39 +0900

    jackson 의존성 추가,웹소켓 데이터 통신 확인

--- tripismProject/src/main/java/com/kh/tripism/travelPlan/chat/model/vo/ChatMessege.java
+++ tripismProject/src/main/java/com/kh/tripism/travelPlan/chat/model/vo/ChatMessege.java
@@ -1,5 +1,25 @@
 package com.kh.tripism.travelPlan.chat.model.vo;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class ChatMessege {
+	private int messageNo; 
+	private int chatroomNo;
+	private int memNo;
+	private String messageText;
+	private String messageDate;
 
 }

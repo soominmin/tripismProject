cbdc85ee8ad2292859ca1041479c6bbbe03ea234
donghyun1597 <donghyun1597@naver.com>
2023-05-17 05:04:55 +0900

    채팅 DB 연동

--- tripismProject/src/main/java/com/kh/tripism/chat/model/dao/ChatDao.java
+++ tripismProject/src/main/java/com/kh/tripism/chat/model/dao/ChatDao.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.tripism.chat.model.vo.ChatMessage;
 import com.kh.tripism.chat.model.vo.ChatRoom;
 
 @Repository
@@ -14,6 +15,11 @@ public class ChatDao {
 	public ArrayList<ChatRoom> selectChatList(SqlSessionTemplate sqlSession,int memNo){
 		
 		return (ArrayList)sqlSession.selectList("chatMapper.chatList",memNo);
+	}
+	
+	public ArrayList<ChatMessage> selectMessageList(SqlSessionTemplate sqlSession, int chatRoomNo){
+		
+		return (ArrayList)sqlSession.selectList("chatMapper.selectMessageList",chatRoomNo);
 	}
 	
 
